3ec083961d912900b9678df5f60591709a14a9a5
Robert Jarzmik <robert.jarzmik@atosorigin.com>
2011-07-19 13:54:37 +0200

    init.el: add NoGnus to the site-lisp directory.

--- init.el
+++ init.el
@@ -77,6 +77,7 @@
 (add-to-list 'load-path (concat user-emacs-directory "site-elisp/ecb-2.40"))
 (add-to-list 'load-path (concat user-emacs-directory "site-elisp/emms-3.0"))
 (add-to-list 'load-path (concat user-emacs-directory "site-elisp/emacs-w3m"))
+(add-to-list 'load-path (concat user-emacs-directory "site-elisp/gnus/lisp"))
 (add-to-list 'load-path (concat user-emacs-directory "elisp"))
 
 ;; Fonts
