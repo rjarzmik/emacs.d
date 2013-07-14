ac44baac227cf9a978e8f2cd8756aba1a5f22720
Robert Jarzmik <robert.jarzmik@free.fr>
2013-07-14 18:14:31 +0200

    java_mode: use 4 space wide tabulations for indentation
    Signed-off-by: Robert Jarzmik <robert.jarzmik@free.fr>

--- elisp/lang/java.el
+++ elisp/lang/java.el
@@ -1,7 +1,8 @@
 ;;; My personnal Java modes
 (add-hook 'java-mode-hook
 	  (lambda ()
-	  (setq indent-tabs-mode nil)
+	  (setq indent-tabs-mode t)
+	  (setq tab-width 4)
 	  (setq c-basic-offset 4)))
 
 ;;; Local Variables:
