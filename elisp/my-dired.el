aaa691f35801edc46dd40ed31fe91fcd7f7c097f
Robert Jarzmik <robert.jarzmik@free.fr>
2014-08-02 21:53:58 +0200

    elisp: my-dired single buffer up dired
    Reuse the same buffer when going in the parent directory with '^' key.
    
    Signed-off-by: Robert Jarzmik <robert.jarzmik@free.fr>

--- elisp/my-dired.el
+++ elisp/my-dired.el
@@ -4,6 +4,13 @@
 
 (put 'dired-find-alternate-file 'disabled nil)
 
+(add-hook 'dired-mode-hook
+	  (lambda ()
+	    (define-key dired-mode-map (kbd "^")
+	      (lambda () (interactive) (find-alternate-file "..")))
+					; was dired-up-directory
+	    ))
+
 (provide 'my-dired)
 
 ;;; Local Variables:
