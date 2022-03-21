bb87ae331cefcd993e96b895ef40e2b7d3692a43
Robert Jarzmik <robert.jarzmik@free.fr>
2022-03-21 13:28:12 +0100

    elisp: my-shells: fix tramp errors
    Tramp errors out saying "tramp-file-name-real-host()" cannot be applied to
    nul. Fix this by using this function only in a remote session.
    
    Signed-off-by: Robert Jarzmik <robert.jarzmik@free.fr>

--- elisp/my-shells.el
+++ elisp/my-shells.el
@@ -5,13 +5,14 @@
 (defun my-shell-hook ()
   "When a shell is spawned, change its name to take into account
 the host where the shell is running on."
-  (let* ((tramp-path (when (tramp-tramp-file-p default-directory)
-		       (tramp-dissect-file-name default-directory)))
-	 (host (tramp-file-name-real-host tramp-path))
-	 (user (if (tramp-file-name-user tramp-path)
-		   (format "%s@" (tramp-file-name-user tramp-path)) ""))
-	 (new-buffer-name (format "*shell:%s%s*" user host)))
-    (rename-buffer new-buffer-name t)))
+  (when (tramp-tramp-file-p default-directory)
+    (let* (
+	   (tramp-path (tramp-dissect-file-name default-directory))
+	   (host (tramp-file-name-host tramp-path))
+	   (user (if (tramp-file-name-user tramp-path)
+		     (format "%s@" (tramp-file-name-user tramp-path)) ""))
+	   (new-buffer-name (format "*shell:%s%s*" user host)))
+      (rename-buffer new-buffer-name t))))
 
 (add-hook 'shell-mode-hook 'my-shell-hook)
 
