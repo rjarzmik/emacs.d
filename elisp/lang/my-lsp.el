bfadd271fc5a41bbda2b4a29bd476fc97465f244
Robert Jarzmik <robert.jarzmik@free.fr>
2022-04-29 20:11:33 +0200

    elisp: lang: my-lsp; add ui and yasnippet
    Add :
    - lsp-ui for the nice ui things
    - yasnippet to add code snippets automatically
    
    Signed-off-by: Robert Jarzmik <robert.jarzmik@free.fr>

--- elisp/lang/my-lsp.el
+++ elisp/lang/my-lsp.el
@@ -17,23 +17,50 @@
   (lsp-auto-guess-root t)                ;; auto guess root
   (lsp-prefer-capf t)                    ;; using `company-capf' by default
   (lsp-keymap-prefix "C-c l")
+  (lsp-eldoc-render-all t)
+  (lsp-idle-delay 0.6)
   :config
   (progn
+    (add-hook 'lsp-mode-hook 'lsp-ui-mode)
     (lsp-register-client
      (make-lsp-client :new-connection (lsp-tramp-connection "clangd")
                       :major-modes '(c-mode c++-mode)
                       :remote? t
                       :server-id 'clangd-remote))))
 
+(use-package lsp-ui
+  :ensure
+  :commands lsp-ui-mode
+  :custom
+  (lsp-ui-peek-always-show t)
+  (lsp-ui-sideline-show-hover t)
+  (lsp-ui-doc-enable nil))
+
+;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+;; inline errors
+(use-package flycheck :ensure)
+
+;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+;; auto-completion and code snippets
 (use-package helm-lsp)
 
+;;; Completion in LSP mode
 (use-package company
   :ensure t
   :hook (prog-mode . company-mode)
-  :bind (:map company-mode-map
-              ([remap completion-at-point] . company-complete))
-  :bind (:map lsp-mode-map
-	      ([remap xref-find-apropos] . helm-lsp-workspace-symbol))
+  :bind
+  (:map company-active-map
+        ("C-n". company-select-next)
+        ("C-p". company-select-previous)
+        ("M-<". company-select-first)
+        ("M->". company-select-last))
+  (:map company-mode-map
+        ("<tab>". tab-indent-or-complete)
+        ("TAB". tab-indent-or-complete))
+  (:map company-mode-map
+        ([remap completion-at-point] . company-complete))
+  (:map lsp-mode-map
+	([remap xref-find-apropos] . helm-lsp-workspace-symbol))
   :custom
   (company-idle-delay 0)
   (company-echo-delay 0)
@@ -41,6 +68,42 @@
   (company-require-match nil)
   (company-tooltip-align-annotations t)
   (company-backends '(company-capf)))
+
+;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+;; setting up yassnippet
+(use-package yasnippet
+  :ensure
+  :config
+  (yas-reload-all)
+  (add-hook 'prog-mode-hook 'yas-minor-mode)
+  (add-hook 'text-mode-hook 'yas-minor-mode))
+
+(defun company-yasnippet-or-completion ()
+  (interactive)
+  (or (do-yas-expand)
+      (company-complete-common)))
+
+(defun check-expansion ()
+  (save-excursion
+    (if (looking-at "\\_>") t
+      (backward-char 1)
+      (if (looking-at "\\.") t
+        (backward-char 1)
+        (if (looking-at "::") t nil)))))
+
+(defun do-yas-expand ()
+  (let ((yas/fallback-behavior 'return-nil))
+    (yas/expand)))
+
+(defun tab-indent-or-complete ()
+  (interactive)
+  (if (minibufferp)
+      (minibuffer-complete)
+    (if (or (not yas/minor-mode)
+            (null (do-yas-expand)))
+        (if (check-expansion)
+            (company-complete-common)
+          (indent-for-tab-command)))))
 
 (provide 'my-lsp)
 
